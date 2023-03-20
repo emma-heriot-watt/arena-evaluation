c8a287dc3557339c2e63d6f0025ac9dc250d0a60
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-03-20 21:08:57 +0000

    fix: use the new arena executable

--- scripts/fetch-arena.sh
+++ scripts/fetch-arena.sh
@@ -3,6 +3,11 @@
 mkdir -p storage/arena
 
 unset AWS_PROFILE
-aws s3 cp s3://alexa-simbot-toolbox-iad-prod/arena-executable/SimbotChallenge.zip /tmp/
-unzip /tmp/SimbotChallenge.zip -d ./storage/arena
-rm -f /tmp/SimbotChallenge.zip
+
+wget -P /tmp/ "https://alexa-arena-executable.s3.us-west-1.amazonaws.com/Arena.zip"
+unzip /tmp/Arena.zip -d ./storage/arena
+rm -f /tmp/Arena.zip
+
+# aws s3 cp s3://alexa-simbot-toolbox-iad-prod/arena-executable/SimbotChallenge.zip /tmp/
+# unzip /tmp/SimbotChallenge.zip -d ./storage/arena
+# rm -f /tmp/SimbotChallenge.zip
