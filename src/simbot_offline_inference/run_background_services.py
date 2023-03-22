bf166941fe5530972d4f78c909443b99ef9bbe2c
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-03-22 11:30:12 +0000

    feat: run eval on single gpu

--- src/simbot_offline_inference/run_background_services.py
+++ src/simbot_offline_inference/run_background_services.py
@@ -28,5 +28,5 @@ if __name__ == "__main__":
         force_download=False,
         run_in_background=False,
         enable_observability=False,
-        is_production=True,
+        is_production=False,
     )
