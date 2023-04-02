1708bd13e3848fbbd2513d09c77724b7b524076c
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-04-02 09:22:44 +0100

    style: fix lint errors

--- src/simbot_offline_inference/metrics.py
+++ src/simbot_offline_inference/metrics.py
@@ -1,10 +1,10 @@
-import json
 import subprocess
 from collections import Counter
 from pathlib import Path
 from typing import Any, Literal, Optional
 from uuid import uuid1
 
+import orjson
 from loguru import logger
 
 
@@ -20,29 +20,29 @@ class SimBotEvaluationMetrics:
     """Store and calculate metrics for the evaluation."""
 
     def __init__(self, evaluation_output_dir: Path, output_metrics_file: Path) -> None:
-        self.output_path = evaluation_output_dir
-        self.output_metrics_file = output_metrics_file
+        self._output_path = evaluation_output_dir
+        self._output_metrics_file = output_metrics_file
 
-        self.games_played = 0
-        self.games_completed = 0
+        self._games_played = 0
+        self._games_completed = 0
 
-        self.subgoals_completed = 0
-        self.total_subgoals = 0
+        self._subgoals_completed = 0
+        self._total_subgoals = 0
 
-        self.mission_groups = []
-        self.games_played_per_mission_group = Counter[str]()
-        self.games_completed_per_mission_group = Counter[str]()
+        self._mission_groups: list[str] = []
+        self._games_played_per_mission_group = Counter[str]()
+        self._games_completed_per_mission_group = Counter[str]()
 
     @property
     def games_failed(self) -> int:
         """Get the total number of games where the agent failed outright."""
-        return self.games_played - self.games_completed
+        return self._games_played - self._games_completed
 
     @property
     def overall_success_rate(self) -> float:
         """Calculate the overall success rate."""
         try:
-            return self.games_completed / self.games_played
+            return self._games_completed / self._games_played
         except ZeroDivisionError:
             return 0
 
@@ -50,7 +50,7 @@ class SimBotEvaluationMetrics:
     def overall_subgoal_completion_rate(self) -> float:
         """Calculate the overall subgoal completion rate."""
         try:
-            return self.subgoals_completed / self.total_subgoals
+            return self._subgoals_completed / self._total_subgoals
         except ZeroDivisionError:
             return 0
 
@@ -59,11 +59,11 @@ class SimBotEvaluationMetrics:
         """Calculate the success rate per mission group."""
         output = {}
 
-        for mission_group in set(self.mission_groups):
+        for mission_group in set(self._mission_groups):
             try:
                 output[mission_group] = (
-                    self.games_completed_per_mission_group[mission_group]
-                    / self.games_played_per_mission_group[mission_group]
+                    self._games_completed_per_mission_group[mission_group]
+                    / self._games_played_per_mission_group[mission_group]
                 )
             except (KeyError, ZeroDivisionError):
                 output[mission_group] = 0
@@ -72,7 +72,7 @@ class SimBotEvaluationMetrics:
 
     def has_mission_been_evaluated(self, mission_name: str) -> bool:
         """Check if the mission has already been evaluated."""
-        return self.output_path.joinpath(f"{mission_name}.json").exists()
+        return self._output_path.joinpath(f"{mission_name}.json").exists()
 
     def add_mission_metrics(
         self,
@@ -84,32 +84,31 @@ class SimBotEvaluationMetrics:
         last_game_state: dict[str, Any],
     ) -> None:
         """Add metrics from a recently evaluated mission."""
-        self.games_played += 1
+        self._games_played += 1
 
         if is_mission_completed:
-            self.games_completed += 1
+            self._games_completed += 1
 
         if mission_group:
-            self.mission_groups.append(mission_group)
-            self.games_played_per_mission_group.update([mission_group])
+            self._mission_groups.append(mission_group)
+            self._games_played_per_mission_group.update([mission_group])
 
             if is_mission_completed:
-                self.games_completed_per_mission_group.update([mission_group])
+                self._games_completed_per_mission_group.update([mission_group])
 
-        self.total_subgoals += len(subgoal_completion_status)
-        self.subgoals_completed += sum(subgoal_completion_status)
+        self._total_subgoals += len(subgoal_completion_status)
+        self._subgoals_completed += sum(subgoal_completion_status)
 
         predicted_actions = [{**action, "id": str(uuid1())} for action in predicted_actions]
 
-        output_file = self.output_path.joinpath(f"{mission_name}.json")
+        output_file = self._output_path.joinpath(f"{mission_name}.json")
         output_results = {
             "predicted_actions": predicted_actions,
             "last_game_state": last_game_state,
         }
-        with open(output_file, "w") as file:
-            json.dump(output_results, file)
+        output_file.write_bytes(orjson.dumps(output_results))
 
-        logger.info(f"Test #{self.games_played} over")
+        logger.info(f"Test #{self._games_played} over")
         logger.info(f"Mission completion status: {is_mission_completed}")
         logger.info(f"Subgoal completion status: {subgoal_completion_status}")
         logger.info(
@@ -123,36 +122,36 @@ class SimBotEvaluationMetrics:
 
     def log_overall_metrics(self) -> None:
         """Log the metrics to the CLI."""
-        logger.info(f"Games played: {self.games_played}")
+        logger.info(f"Games played: {self._games_played}")
         logger.info(f"Overall success rate: {self.overall_success_rate}")
         logger.info(f"Overall subgoal completion rate: {self.overall_subgoal_completion_rate}")
         logger.info(f"Success rate per mission group: {self.success_rate_per_mission_group}")
 
         output = {
-            "games_played": self.games_played,
-            "games_completed": self.games_completed,
-            "subgoals_completed": self.subgoals_completed,
-            "total_subgoals": self.total_subgoals,
-            "mission_groups": list(set(self.mission_groups)),
-            "games_played_per_mission_group": self.games_played_per_mission_group,
-            "games_completed_per_mission_group": self.games_completed_per_mission_group,
+            "games_played": self._games_played,
+            "games_completed": self._games_completed,
+            "subgoals_completed": self._subgoals_completed,
+            "total_subgoals": self._total_subgoals,
+            "mission_groups": list(set(self._mission_groups)),
+            "games_played_per_mission_group": self._games_played_per_mission_group,
+            "games_completed_per_mission_group": self._games_completed_per_mission_group,
             "success_rate": self.overall_success_rate,
             "subgoal_completion_rate": self.overall_subgoal_completion_rate,
             "success_rate_per_mission": self.success_rate_per_mission_group,
         }
 
-        with open(self.output_metrics_file, "w") as metrics_file:
-            json.dump(output, metrics_file)
+        self._output_metrics_file.write_bytes(orjson.dumps(output))
 
     def send_to_s3(self) -> None:
-        logger.info("Uploading to S3")
+        """Upload the results to S3."""
+        logger.info("Uploading to S3...")
         subprocess.run(
-            f"aws s3 cp {str(self.output_path)} s3://emma-simbot/results/simbot-eval-ai/missions/ --recursive",
+            f"aws s3 cp {str(self._output_path)} s3://emma-simbot/results/simbot-eval-ai/missions/ --recursive",
             shell=True,
             check=True,
         )
         subprocess.run(
-            f"aws s3 cp {str(self.output_metrics_file)} s3://emma-simbot/results/simbot-eval-ai/metrics_{str(uuid1())}.json",
+            f"aws s3 cp {str(self._output_metrics_file)} s3://emma-simbot/results/simbot-eval-ai/metrics_{str(uuid1())}.json",
             shell=True,
             check=True,
         )
