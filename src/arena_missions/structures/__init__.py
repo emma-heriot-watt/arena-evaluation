2691344887a0f40c98ff11caec2431787e7a6f55
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-04-11 09:39:27 +0100

    chore: fix __init__ imports for structures

--- src/arena_missions/structures/__init__.py
+++ src/arena_missions/structures/__init__.py
@@ -3,4 +3,9 @@ from arena_missions.structures.high_level_key import HighLevelKey
 from arena_missions.structures.mission import Mission, MissionTrajectory
 from arena_missions.structures.object_id import ObjectId, ObjectInstanceId
 from arena_missions.structures.required_object import RequiredObject, RequiredObjectState
-from arena_missions.structures.task_goal import ObjectGoalStateExpression, TaskGoal
+from arena_missions.structures.task_goal import (
+    ObjectGoalState,
+    ObjectGoalStateExpression,
+    ObjectGoalStateRelation,
+    TaskGoal,
+)
