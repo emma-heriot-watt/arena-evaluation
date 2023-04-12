133b04938b89eff8e864fd17fa35b6702b424e21
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-04-12 20:56:45 +0100

    fix: get rid of the progress bar
    it might be causing crashes. i don't know. we don't need it. it doesn't matter so long as it runs.

--- src/simbot_offline_inference/arena_evaluator.py
+++ src/simbot_offline_inference/arena_evaluator.py
@@ -3,7 +3,6 @@ from loguru import logger
 from arena_missions.structures import CDF, MissionTrajectory
 from simbot_offline_inference.inference_controller import SimBotInferenceController
 from simbot_offline_inference.metrics import SimBotEvaluationMetrics
-from simbot_offline_inference.progress import ArenaEvaluatorProgressTracker
 
 
 EXPERIENCE_HUB_HEALTHCHECK_ATTEMPTS = 40
@@ -24,36 +23,25 @@ class SimBotArenaEvaluator:
 
         self._restart_arena_after_num_sessions = restart_arena_after_num_sessions
 
-        self._progress = ArenaEvaluatorProgressTracker()
-
     def run_evaluation(self, trajectories: list[MissionTrajectory]) -> None:
         """Run the evaluation on all the test data."""
-        self._progress.start_new_evaluation(len(trajectories))
-
         trajectory_batches = self._create_trajectory_batches(trajectories)
 
-        with self._progress.display():
-            for batch in trajectory_batches:
-                self.run_evaluation_batch(batch)
+        for batch in trajectory_batches:
+            with self._inference_controller:
+                for instance in batch:
+                    self.run_evaluation_step(instance)
+
+                logger.info("Completed batch. Restarting Arena...")
 
             logger.info("Finished evaluation!")
 
         self._evaluation_metrics.log_overall_metrics()
         self._evaluation_metrics.send_to_s3()
 
-    def run_evaluation_batch(self, batch: list[MissionTrajectory]) -> None:
-        """Run the evaluation on a batch of test data."""
-        with self._inference_controller:
-            for instance in batch:
-                self.run_evaluation_step(instance)
-                self._progress.finish_trajectory()
-
-            logger.info("Completed batch. Restarting Arena...")
-
     def run_evaluation_step(self, trajectory: MissionTrajectory) -> None:
         """Run the evaluation on a single instance of the test data."""
         session_id = trajectory.session_id
-        self._progress.start_new_session(num_utterances=len(trajectory.utterances))
 
         if self._evaluation_metrics.has_mission_been_evaluated(session_id):
             logger.info(f"Mission ({session_id}) has already been evaluated. Skipping...")
@@ -70,7 +58,6 @@ class SimBotArenaEvaluator:
                 session_id, utterance
             )
             actions_for_session.extend(actions_for_utterance)
-            self._progress.sent_utterance()
 
         # Check goal status and get results
         logger.debug("Calculating metrics for test")
