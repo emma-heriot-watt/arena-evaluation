0f9fcdd800e30cc0e94a047dd37ec9a43dffdc42
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-04-12 20:35:24 +0100

    feat: restart the arena after every 10 sessions
    hopeflly to reduce the number of crashes/hangs

--- src/simbot_offline_inference/arena_evaluator.py
+++ src/simbot_offline_inference/arena_evaluator.py
@@ -16,9 +16,13 @@ class SimBotArenaEvaluator:
         self,
         inference_controller: SimBotInferenceController,
         evaluation_metrics: SimBotEvaluationMetrics,
+        *,
+        restart_arena_after_num_sessions: int = 10,
     ) -> None:
         self._inference_controller = inference_controller
         self._evaluation_metrics = evaluation_metrics
+
+        self._restart_arena_after_num_sessions = restart_arena_after_num_sessions
 
         self._progress = ArenaEvaluatorProgressTracker()
 
@@ -26,22 +30,30 @@ class SimBotArenaEvaluator:
         """Run the evaluation on all the test data."""
         self._progress.start_new_evaluation(len(trajectories))
 
-        with self._progress.display():
-            with self._inference_controller:
-                self._progress.finish_unity_check()
-                for instance in trajectories:
-                    self.run_evaluation_step(instance)
-                    self._progress.finish_trajectory()
+        trajectory_batches = self._create_trajectory_batches(trajectories)
 
-                logger.info("Finished evaluation!")
+        with self._progress.display():
+            for batch in trajectory_batches:
+                self.run_evaluation_batch(batch)
+
+            logger.info("Finished evaluation!")
 
         self._evaluation_metrics.log_overall_metrics()
         self._evaluation_metrics.send_to_s3()
 
+    def run_evaluation_batch(self, batch: list[MissionTrajectory]) -> None:
+        """Run the evaluation on a batch of test data."""
+        with self._inference_controller:
+            for instance in batch:
+                self.run_evaluation_step(instance)
+                self._progress.finish_trajectory()
+
+            logger.info("Completed batch. Restarting Arena...")
+
     def run_evaluation_step(self, trajectory: MissionTrajectory) -> None:
         """Run the evaluation on a single instance of the test data."""
         session_id = trajectory.session_id
-        self._progress.start_new_trajectory(session_id, num_utterances=len(trajectory.utterances))
+        self._progress.start_new_session(session_id, num_utterances=len(trajectory.utterances))
 
         if self._evaluation_metrics.has_mission_been_evaluated(session_id):
             logger.info(f"Mission ({session_id}) has already been evaluated. Skipping...")
@@ -49,18 +61,16 @@ class SimBotArenaEvaluator:
 
         logger.info(f"Running evaluation for '{session_id}'")
 
-        self._progress.loading_cdf()
         self.prepare_cdf_in_arena(trajectory.cdf)
-        self._progress.arena_ready_for_utterance()
 
         actions_for_session = []
 
         for utterance in trajectory.utterances:
+            self._progress.sent_utterance()
             actions_for_utterance = self._inference_controller.handle_utterance(
                 session_id, utterance
             )
             actions_for_session.extend(actions_for_utterance)
-            self._progress.sent_utterance()
 
         # Check goal status and get results
         logger.debug("Calculating metrics for test")
@@ -85,3 +95,15 @@ class SimBotArenaEvaluator:
         logger.debug("Verifying Experience Hub is healthy")
         if not self._inference_controller.healthcheck():
             raise AssertionError("The Experience Hub is not healthy.")
+
+    def _create_trajectory_batches(
+        self, trajectories: list[MissionTrajectory]
+    ) -> list[list[MissionTrajectory]]:
+        batched_trajectories = []
+
+        for idx in range(0, len(trajectories), self._restart_arena_after_num_sessions):
+            batched_trajectories.append(
+                trajectories[idx : idx + self._restart_arena_after_num_sessions]
+            )
+
+        return batched_trajectories
--- src/simbot_offline_inference/progress.py
+++ src/simbot_offline_inference/progress.py
@@ -1,18 +1,5 @@
-# Things to track:
-# [ ] Number of trajectories that completed / total trajectories
-# [ ] Current mission name
-# [ ] Unity instance status (running, checking X/250)
-# [ ] Trajectory phase:
-#       Loading CDF (spinning)
-#       Sending dummy actions (X/10)
-#       Sending utterance (X/Y)
-
-from rich.columns import Columns
-from rich.console import Group
 from rich.live import Live
-from rich.panel import Panel
 from rich.progress import BarColumn, MofNCompleteColumn, Progress, TimeElapsedColumn
-from rich.text import Text
 
 
 class ArenaEvaluatorProgressTracker:
@@ -27,122 +14,36 @@ class ArenaEvaluatorProgressTracker:
             expand=True,
         )
 
-        self._trajectory_count_task = self.progress.add_task(
-            description="Running trajectories",
-            start=False,
+        self._overall_trajectory_task = self.progress.add_task(
+            description="Overall progress", start=False, total=None
         )
-
-        self._unity_status_task = self.progress.add_task(
-            description="Check unity is running",
-            start=False,
-            total=1,
-            visible=False,
+        self._current_trajectory_task = self.progress.add_task(
+            description="Current progress", start=False, total=None, visible=False
         )
-
-        self._trajectory_progress = Progress(
-            "{task.description}", BarColumn(bar_width=None), MofNCompleteColumn(), expand=True
-        )
-        self._loading_cdf_task = self._trajectory_progress.add_task(
-            description="Loading CDF",
-            total=10,
-            start=False,
-            visible=False,
-        )
-        self._sent_utterance_task = self._trajectory_progress.add_task(
-            description="Sending utterances", start=False
-        )
-
-        self._current_mission_name: str = ""
-        self._completed_mission_names: list[str] = []
 
     def start_new_evaluation(self, total_trajectories: int) -> None:
         """Start a new evaluation."""
-        self.progress.update(self._trajectory_count_task, total=total_trajectories)
-        self.progress.start_task(self._trajectory_count_task)
+        self.progress.update(self._overall_trajectory_task, total=total_trajectories)
+        self.progress.start_task(self._overall_trajectory_task)
 
-    def start_unity_check(self, total: int) -> None:
-        """Start the Unity instance check progress."""
-        self.progress.start_task(self._unity_status_task)
-        self.progress.update(
-            self._unity_status_task, total=total, description="Checking Unity instance"
-        )
-
-    def sent_another_unity_check(self) -> None:
-        """Update progress when sending another check for the Unity instance."""
-        self.progress.advance(self._unity_status_task)
-
-    def finish_unity_check(self) -> None:
-        """Finish the Unity instance check progress."""
-        self.progress.stop_task(self._unity_status_task)
-        self.progress.update(
-            self._unity_status_task,
-            description="Unity instance is running",
-            completed=self.progress.tasks[self._unity_status_task].completed,
-        )
-
-    def start_new_trajectory(
-        self, mission_name: str, max_num_dummy_actions: int = 10, num_utterances: int = 1
-    ) -> None:
+    def start_new_session(self, session_id: str, num_utterances: int = 1) -> None:
         """Reset the trajectory progress section for the new trajectory."""
-        self._current_mission_name = mission_name
-        self._trajectory_progress.reset(
-            self._loading_cdf_task, total=max_num_dummy_actions, start=False
+        self.progress.reset(
+            self._current_trajectory_task, start=False, total=num_utterances, visible=True
         )
-        self._trajectory_progress.reset(
-            self._sent_utterance_task, total=num_utterances, start=False
-        )
+        self.progress.update(self._current_trajectory_task, description=f"Running {session_id}")
 
-    def loading_cdf(self) -> None:
-        """Update trajectory progress when loading the CDF in the arena."""
-        self._trajectory_progress.start_task(self._loading_cdf_task)
-
-    def sent_dummy_actions(self, count: int = 1) -> None:
-        """Update trajectory progress when sending dummy actions."""
-        self._trajectory_progress.advance(self._loading_cdf_task, count)
-
-    def arena_ready_for_utterance(self) -> None:
-        """Update trajectory progress when the arena is ready for an utterance."""
-        self._trajectory_progress.update(
-            self._loading_cdf_task,
-            total=self._trajectory_progress.tasks[self._loading_cdf_task].completed,
-        )
-        self._trajectory_progress.stop_task(self._loading_cdf_task)
-
-        self._trajectory_progress.start_task(self._sent_utterance_task)
-
-    def sent_utterance(self, count: int = 1) -> None:
+    def sent_utterance(self) -> None:
         """Update trajectory progress when sending an utterance."""
-        # If the loading CDF task is still running, stop it
-        self._trajectory_progress.advance(self._sent_utterance_task, count)
+        if not self.progress.tasks[self._current_trajectory_task].started:
+            self.progress.start_task(self._current_trajectory_task)
+        self.progress.advance(self._current_trajectory_task)
 
     def finish_trajectory(self) -> None:
-        """Finish the trajectory progress section."""
-        self._trajectory_progress.stop_task(self._sent_utterance_task)
-        self.progress.advance(self._trajectory_count_task)
-        self._completed_mission_names.append(self._current_mission_name)
+        """Finish the trajectory progress."""
+        self.progress.stop_task(self._current_trajectory_task)
+        self.progress.advance(self._overall_trajectory_task)
 
     def display(self) -> Live:
         """Display the progress tracker."""
-        return Live(self._create_display())
-
-    def _create_display(self) -> Panel:
-        all_completed_missions = [
-            Text(mission_name) for mission_name in sorted(set(self._completed_mission_names))
-        ]
-
-        table_of_completed = Columns(all_completed_missions, equal=True, expand=True)
-
-        return Panel(
-            Group(
-                self.progress,
-                self._trajectory_progress,
-                Panel(
-                    table_of_completed,
-                    title="Completed missions",
-                    border_style="green",
-                ),
-            ),
-            title="Progress",
-            border_style="yellow",
-            padding=(1, 5),
-        )
+        return Live(self.progress)
