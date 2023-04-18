16d6608e41b289373b1b67bfc9b9ee83058ddd59
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-04-18 16:08:46 +0100

    chore(deps): update emma-experience-hub dep

--- src/simbot_offline_inference/commands/run_background_services.py
+++ src/simbot_offline_inference/commands/run_background_services.py
@@ -1,9 +1,4 @@
 from emma_experience_hub.commands.simbot.cli import (
-    OBSERVABILITY_COMPOSE_PATH,
-    SERVICE_REGISTRY_PATH,
-    SERVICES_COMPOSE_PATH,
-    SERVICES_PROD_COMPOSE_PATH,
-    SERVICES_STAGING_COMPOSE_PATH,
     run_background_services as run_exp_hub_background_services,
 )
 from simbot_offline_inference.settings import Settings
@@ -15,21 +10,10 @@ def run_background_services() -> None:
     settings.put_settings_in_environment()
 
     run_exp_hub_background_services(
-        service_registry_path=SERVICE_REGISTRY_PATH,
-        services_docker_compose_path=settings.experience_hub_dir.joinpath(SERVICES_COMPOSE_PATH),
-        staging_services_docker_compose_path=settings.experience_hub_dir.joinpath(
-            SERVICES_STAGING_COMPOSE_PATH
-        ),
-        production_services_docker_compose_path=settings.experience_hub_dir.joinpath(
-            SERVICES_PROD_COMPOSE_PATH
-        ),
-        observability_services_docker_compose_path=settings.experience_hub_dir.joinpath(
-            OBSERVABILITY_COMPOSE_PATH
-        ),
         model_storage_dir=settings.models_dir,
-        download_models=True,
-        force_download=False,
+        force_download_models=False,
         run_in_background=False,
-        enable_observability=False,
-        is_production=False,
+        observability=False,
+        num_gpus=2,
+        offline_evaluation=True,
     )
